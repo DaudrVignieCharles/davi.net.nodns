3f21daeb8f779b21a5d405bbd87391e2e14f80cb
Daudré-Vignier Charles <missaalias@mail.ru>
2017-07-14 04:39:59 +0200

    update

---
+++ get_ip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*-coding:UTF8 -*
+
+from xml.etree import ElementTree
+
+from sources.pastebinAPI import Api
+from sources.settings_ import devKey, userKey, pasteName
+
+api = Api(devKey, userKey)
+
+paste_list = api.list_paste(100).text
+xml = ElementTree.fromstring('<list>' + paste_list + '</list>')
+pasteKey = xml.findall(".//paste[paste_title='{0}']/paste_key".format(pasteName))[0].text
+print(api.show_paste(pasteKey).text)
